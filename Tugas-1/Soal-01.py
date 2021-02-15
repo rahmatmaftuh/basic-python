edc79740507d6bd561dbebe043eb83752166dc66
rahmatmaftuh <78604097+rahmatmaftuh@users.noreply.github.com>
2021-02-15 14:41:51 +0700

    Create Soal-01.py

---
+++ Tugas-1/Soal-01.py
@@ -0,0 +1,5 @@
+Nama = input("Siapa nama anda? ") #variabel nama untuk menandai input berupa sebuah nama
+Umur = int(input("Berapa umur anda? ")) #variabel umur untuk menandai input berupa umur dan bertipe integer
+Tinggi = float(input("Berapa tinggi anda dalam cm? ")) #variable tinggi untuk menandai berupa tinggi dan bertipe float
+
+print("Nama saya {}, umur saya {} tahun dan tinggi saya {} cm".format(Nama,Umur,Tinggi))
