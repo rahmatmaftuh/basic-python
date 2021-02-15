f4a92db58357f8e2f2ae4c50ad405cbe5a60a74c
rahmatmaftuh <78604097+rahmatmaftuh@users.noreply.github.com>
2021-02-15 14:42:19 +0700

    Add files via upload

---
+++ Tugas-1/Soal-2.py
@@ -0,0 +1,4 @@
+print("Selamat datang di program menghitung luas lingkaran!")
+jari_jari = float(input("Masukkan jari-jari yang kamu inginkan dalam cm? "))
+rumus_lingkaran = (22/7) * jari_jari * jari_jari
+print("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm\u00b2".format(jari_jari,rumus_lingkaran))
